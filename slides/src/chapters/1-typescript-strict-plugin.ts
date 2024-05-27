9ea7d0b6f6fce9561543a22a21c41d096d547fea
Aurélien Loyer <aur.loy@gmail.com>
2024-05-27 13:32:29 +0200

    feat: add chapter 1 scripts exemple ✨

--- slides/src/chapters/1-typescript-strict-plugin.ts
+++ slides/src/chapters/1-typescript-strict-plugin.ts
@@ -1,9 +1,9 @@
 import { CodeFromText } from "../slide-framework/block/code/code-from-text.block";
 import { Slide } from "../slide-framework/block/slide.block";
 import { Subtitle } from "../slide-framework/block/subtitle.block";
-import { Chapter } from "../slide/chapter.slide";
 import { TitleSlide } from "../slide/title.slide";
 import { CodeStepsBuilder } from "../slide-framework/block/code/code.block";
+import { Chapter } from "../slide/chapter.slide";
 
 const pluginSlide0 = new TitleSlide(
   "<i>Strictness</i> won't save you when the bullets fly",
@@ -42,4 +42,19 @@ const pluginSlide1 = new Slide([
     ),
 ]);
 
-export const chapter1Slide = Chapter.withSlides([pluginSlide0, pluginSlide1]);
+const pluginSlide2 = new Slide([
+  Subtitle.withText("🎉 Enjoy the benefits of progressive strictness"),
+  Subtitle.withText("<br>"),
+  Subtitle.withText("🙅‍♂️ No more excuses"),
+  Subtitle.withText("<br>"),
+  new CodeFromText(
+    `{
+  "scripts": {
+    ...,
+    "typecheck": "tsc && tsc-strict",
+  },
+}`
+  ),
+]);
+
+export const chapter1Slide = Chapter.withSlides([pluginSlide0, pluginSlide1, pluginSlide2]);
