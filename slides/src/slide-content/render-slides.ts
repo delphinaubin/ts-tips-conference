649558d14cd634ed26eb85f2780c266143e0e988
delphinaubin <delphin_5@msn.com>
2024-06-04 00:31:37 +0200

    fix: remove useless import

--- slides/src/slide-content/render-slides.ts
+++ slides/src/slide-content/render-slides.ts
@@ -7,7 +7,6 @@ import { CompiledCodeSlide } from "../slide/compiled-code.slide";
 import { ResumeSlide } from "../slide/resume.slide";
 import { TsFeatureSlide } from "../slide/ts-feature.slide";
 import { ListSlide } from "../slide/list.slide";
-import { ImageSlide } from "../slide/image.slide";
 
 export function renderSlides(slides: SlideContent[]): Slide[] {
   return slides.map(
