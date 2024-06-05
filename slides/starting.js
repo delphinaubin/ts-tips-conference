193561f4bd1a6e0be9c417ecaddfa5ce1203d110
Aurélien Loyer <aur.loy@gmail.com>
2024-06-05 23:39:40 +0200

    chore: update starting script with conditional setTimeout 🔧

--- slides/starting.js
+++ slides/starting.js
@@ -1,10 +1,17 @@
 import ora from 'ora';
+import * as fs from 'fs';
 
-const spinner = ora('Starting slide framework...')
-    .start();
-
+const spinner = ora('Starting slide framework...').start();
 spinner.color = 'yellow';
 
-setTimeout(() => {
-    spinner.info('Slide framework ready!');
-}, 4000);
+if (fs.existsSync('.run')) {
+    setTimeout(() => {
+        spinner.succeed('Slide framework started.');
+    }, 1000);
+} else {
+    fs.writeFileSync('.run', '');
+
+    setTimeout(() => {
+        spinner.fail('Slide framework failed to start.');
+    }, 4000);
+}
